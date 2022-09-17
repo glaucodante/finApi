905bceb03d3096f8b25d7d481644ad84dbb9045a
Glauco Dante <108493608+glaucodante@users.noreply.github.com>
2022-09-17 10:03:27 -0300

    updating finApi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 // v4 - vai gerar numeros randomicos aleatorios
 const { v4: uuidv4 } = require("uuid");
@@ -9,6 +10,36 @@ app.use(express.json());
 // Simulação de banco de dados
 const customers = [];
 
+// Middleware
+// next = irá definir se o middleware vai prosseguir ou não
+function verifyIfExistsAcountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    // find irá retornar um valor
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    }
+
+    request.customer = customer;
+
+    return next();
+
+};
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === "credit") {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 /** 
  * cpf - string
  * name - string
@@ -16,9 +47,10 @@ const customers = [];
  * statement (extrato, lançamentos) - array => []
 */
 
+// conta do cliente
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
-
+    // some irá retornar um boolean
     const customerAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf
     );
@@ -38,6 +70,73 @@ app.post('/account', (request, response) => {
     });
 
     return response.status(201).send();
+});
+
+// app.use(verifyIfExistsAcountCPF);
+
+// extrato bancário
+app.get('/statement/', verifyIfExistsAcountCPF, (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAcountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    // verifica se a conta é válida ou não
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+    // inserindo a operação dentro do customer
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
+});
+
+app.post('/withdraw', verifyIfExistsAcountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
+
+});
+
+app.get('/statement/date', verifyIfExistsAcountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    // transformar a data, independente do horário da transação 
+    const dateFormat = new Date(date + "00:00");
+    // transformando para esse formato 10/10/2000
+    const statement = customer.statement.filter(
+        (statement) =>
+            statement.created_at.toDateString() ===
+            new Date(dateFormat).toDateString());
+
+    return response.json(customer.statement);
 });
 
 app.listen(2000);
